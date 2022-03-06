e1b5311609ab0044857641d9ace6b4d9732b95df
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-06 23:34:09 +0100

    Add bean scopes

---
+++ src/main/java/se/iths/springloppis/service/BeanScopesDemo.java
@@ -0,0 +1,26 @@
+package se.iths.springloppis.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Scope("prototype")
+public class BeanScopesDemo {
+
+    Logger logger = LoggerFactory.getLogger(BeanScopesDemo.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("BeanScopesDemo bean is alive!!");
+    }
+
+    // Singleton - default bean type in @Component
+    // Prototype - Ny böna vid varje tillfälle den refereras
+    // Session - används enbart i webbsammanhang. En instans per 'session'
+    // Request - används enbart i webbsammanhang. En instans per 'request'
+
+}
