791245c5b5283d6fff2c6124fbbd2dcb2c5bd6c1
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-10 09:24:31 +0100

    Add comments to configure method

--- src/main/java/se/iths/springloppis/security/SecurityConfig.java
+++ src/main/java/se/iths/springloppis/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package se.iths.springloppis.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,25 +26,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider());
-    }
+    // if we have only one userDetailServices we don't need to override this method
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) {
+//        auth.authenticationProvider(authenticationProvider());
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()   // temporarily disable csrf protection for demo purposes
-                .httpBasic()
+                .httpBasic()        // type of login we want to have - HTTP Basic in this case
                 .and()
                 .authorizeRequests()
                 .antMatchers("/", "/home", "/users/signup").permitAll() //hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+                .formLogin()    // form login for Thymeleaf
                 .loginPage("/login").permitAll()
                 .and()
-                .logout()   // can create a logout.html template
+                .logout()   // one can create a logout.html template
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .permitAll();
